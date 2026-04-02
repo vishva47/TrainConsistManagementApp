cc5307c8124d404954fc123560e1dd402eca0f6b
vishva47 <vishvarethinam123@gmail.com>
2026-04-02 14:29:06 +0530

    UC5: LinkedHashSet Order

--- src/UseCase5.java
+++ src/UseCase5.java
@@ -4,15 +4,10 @@ import java.util.Set;
 public class UseCase5 {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-
         Set<String> bogies = new LinkedHashSet<>();
         bogies.add("Sleeper");
         bogies.add("AC Chair");
         bogies.add("First Class");
-        bogies.add("Pantry Car");
-        bogies.add("Sleeper"); // duplicate - ignored
-
-        System.out.println("Bogies (Insertion Order Preserved): " + bogies);
-        System.out.println("Total Bogies: " + bogies.size());
+        System.out.println("Bogies (Insertion Order): " + bogies);
     }
 }
