22cce5422e534c7e093bc7d26ae1798fdc489ed3
vishva47 <vishvarethinam123@gmail.com>
2026-04-02 14:28:58 +0530

    UC1: Initialize Train

--- src/UseCase1.java
+++ src/UseCase1.java
@@ -5,9 +5,7 @@ public class UseCase1 {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
         System.out.println("Initializing train consist...");
-
         List<String> bogies = new ArrayList<>();
-
         System.out.println("Initial Bogie Count: " + bogies.size());
         System.out.println("Train consist initialized successfully.");
     }
